388cd4d84f2491800b9d0863ef65503436ef3671
zzyzy <leezhenzhi@gmail.com>
2016-09-19 21:35:34 +1000

    Created a environment (room)

--- OgreGame2/MainApplication.cpp
+++ OgreGame2/MainApplication.cpp
@@ -11,7 +11,8 @@
 #include "HandGun.hpp"
 #include "SniperRifle.hpp"
 
-MainApplication::MainApplication()
+MainApplication::MainApplication() :
+	mSniperScopeOverlay(nullptr)
 {
 	mBulletContext.Setup();
 }
@@ -100,107 +101,253 @@ void MainApplication::setupScene(Ogre::SceneManager* const sceneMgr)
 	auto* light = sceneMgr->createLight("MainLight");
 	light->setPosition(0, 0, 0);
 
-	Ogre::Plane plane(Ogre::Vector3::UNIT_Y, 0);
-	/*
-	Ogre::MeshManager::getSingleton().createPlane(
-	    "plane",
-	    Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
-	    plane,
-	    500, 500, 20, 20,
-	    true,
-	    1, 5, 5,
-	    Ogre::Vector3::UNIT_Z
-	);
-	*/
-	Ogre::MeshManager::getSingleton().createPlane("ground", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, plane,
-	                                              100, 100, 20, 20, true, 1, 5, 5, Ogre::Vector3::UNIT_Z);
+	// Setup ground, walls and ceiling (room)
+	{
+		// Setup graphics
+		Ogre::Plane plane(Ogre::Vector3::UNIT_Y, 0);
+		Ogre::Plane wall(Ogre::Vector3::UNIT_Z, 0);
 
-	auto* groundEntity = sceneMgr->createEntity("ground");
-	sceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(groundEntity);
-	groundEntity->setMaterialName("Examples/Rockwall");
+		Ogre::MeshManager::getSingleton().createPlane(
+			"plane",
+			Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+			plane,
+			100, 100, 20, 20,
+			true,
+			1, 5, 5,
+			Ogre::Vector3::UNIT_Z
+		);
+		Ogre::MeshManager::getSingleton().createPlane(
+			"wall",
+			Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+			wall,
+			100, 40, 20, 20,
+			true,
+			1, 5, 5,
+			Ogre::Vector3::UNIT_Y
+		);
 
-	// Create the ground object shape
-	btBoxShape* groundShape = new btBoxShape(btVector3(50.0f, 5.0f, 50.0f));
+		// Ground
+		auto *groundEntity = mSceneMgr->createEntity("plane");
+		mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(groundEntity);
+		groundEntity->setMaterialName("Examples/GrassFloor");
 
-	// Set the object's transformation
-	btTransform groundTransform;
-	groundTransform.setIdentity();
-	groundTransform.setOrigin(btVector3(0, -5, 0));
+		// Ceiling
+		auto *ceilingEntity = mSceneMgr->createEntity("plane");
+		auto *ceilingNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+		ceilingNode->attachObject(ceilingEntity);
+		ceilingEntity->setMaterialName("Examples/BeachStones");
+		ceilingNode->rotate(Ogre::Vector3::UNIT_X, Ogre::Degree(180));
+		ceilingNode->translate(Ogre::Vector3(0, 40, 0));
 
-	Ogre::Entity* cube = mSceneMgr->createEntity("Ground", "cube.mesh");
-	Ogre::SceneNode* node = mSceneMgr->getRootSceneNode()->createChildSceneNode();
-	node->attachObject(cube);
-	node->scale(1.0f, 0.1f, 1.0f);
-	node->translate(0.0f, -5.0f, 0.0f);
+		// Place walls
+		Ogre::Entity *walls[4];
+		Ogre::SceneNode *wallNodes[4];
 
-	// Create the rigid body
-	mBulletContext.CreateRigidBody(0.0f, groundTransform, groundShape, node);
-	node->setVisible(false);
+		// Front wall
+		walls[0] = mSceneMgr->createEntity("wall");
+		wallNodes[0] = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+		wallNodes[0]->attachObject(walls[0]);
+		walls[0]->setMaterialName("Examples/Rockwall");
+		wallNodes[0]->translate(Ogre::Vector3(0, 20, -50));
 
-	auto fpsPlayer = sceneMgr->getRootSceneNode()->createChildSceneNode("FPSPlayer");
-	auto cameraNode = fpsPlayer->createChildSceneNode("CameraNode");
-	cameraNode->attachObject(mCamera);
-	mCamera->setPosition(0.0f, 0.0f, 0.0f);
-	cameraNode->setPosition(0.0f, 10.0f, 0.0f);
-	mFPSController.Attach(cameraNode, fpsPlayer);
+		// Right wall
+		walls[1] = mSceneMgr->createEntity("wall");
+		wallNodes[1] = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+		wallNodes[1]->attachObject(walls[1]);
+		walls[1]->setMaterialName("Examples/Rockwall");
+		wallNodes[1]->rotate(Ogre::Vector3::UNIT_Y, Ogre::Degree(270));
+		wallNodes[1]->translate(Ogre::Vector3(50, 20, 0));
 
-	mWeapon.SetState(new BallShooter(mSceneMgr, mCamera, &mBulletContext));
+		// Left wall
+		walls[2] = mSceneMgr->createEntity("wall");
+		wallNodes[2] = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+		wallNodes[2]->attachObject(walls[2]);
+		walls[2]->setMaterialName("Examples/Rockwall");
+		wallNodes[2]->rotate(Ogre::Vector3::UNIT_Y, Ogre::Degree(90));
+		wallNodes[2]->translate(Ogre::Vector3(-50, 20, 0));
 
-	// Create a manual object for 2D
-	Ogre::ManualObject* manual = mSceneMgr->createManualObject("manual");
-
-	// Use identity view/projection matrices
-	manual->setUseIdentityProjection(true);
-	manual->setUseIdentityView(true);
-
-	manual->begin("BaseWhiteNoLighting", Ogre::RenderOperation::OT_LINE_LIST);
-
-	manual->position(-0.05, 0.0, 0.0);
-	manual->position(0.05, 0.0, 0.0);
-	manual->position(0.0, -0.05, 0.0);
-	manual->position(0.0, 0.05, 0.0);
-
-	manual->end();
-
-	// Use infinite AAB to always stay visible
-	Ogre::AxisAlignedBox aabInf;
-	aabInf.setInfinite();
-	manual->setBoundingBox(aabInf);
-
-	// Render just before overlays
-	manual->setRenderQueueGroup(Ogre::RENDER_QUEUE_OVERLAY - 1);
-
-	// Attach to scene
-	mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(manual);
-
-	// Get the pointer to the overlay manager
-	Ogre::OverlayManager* overlayManager = Ogre::OverlayManager::getSingletonPtr();
-
-	// Create an overlay
-	mSniperScopeOverlay = overlayManager->create("Overlay");
+		// Back wall
+		walls[3] = mSceneMgr->createEntity("wall");
+		wallNodes[3] = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+		wallNodes[3]->attachObject(walls[3]);
+		walls[3]->setMaterialName("Examples/Rockwall");
+		wallNodes[3]->rotate(Ogre::Vector3::UNIT_Y, Ogre::Degree(180));
+		wallNodes[3]->translate(Ogre::Vector3(0, 20, 50));
 	
-	// Load an image into Ogre's texture manager
-	Ogre::Image image;
-	image.load("sniper-scope-cropped.png", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
-	Ogre::TextureManager::getSingleton().loadImage("LogoTexture", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, image, Ogre::TEX_TYPE_2D, 0, 1.0f);
+		// Setup rigidbodies
+		btTransform transform;
+		btVector3 origin;
+		btQuaternion rotation;
 
-	// Already loaded to texture, no longer need image
-	image.freeMemory();
+		// Dummy entity for rigidbody
+		Ogre::Entity* cube = mSceneMgr->createEntity("Ground", "cube.mesh");
+		Ogre::SceneNode* node = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+		node->attachObject(cube);
+		node->scale(1.0f, 0.1f, 1.0f);
+		node->translate(0.0f, -5.0f, 0.0f);
+		node->setVisible(false);
 
-	// Create a material and assign the texture to the material
-	Ogre::MaterialPtr materialPtr = Ogre::MaterialManager::getSingleton().create("ImageMaterial", "General");
-	materialPtr->getTechnique(0)->getPass(0)->createTextureUnitState("LogoTexture");
-	materialPtr->getTechnique(0)->getPass(0)->setSceneBlending(Ogre::SceneBlendType::SBT_TRANSPARENT_ALPHA);
+		// Ground rigidbody
+		btBoxShape* groundShape = new btBoxShape(btVector3(50.0f, 5.0f, 50.0f));
+		transform.setIdentity();
+		transform.setOrigin(btVector3(0, -5, 0));
+		mBulletContext.CreateRigidBody(0.0f, transform, groundShape, node);
 
-	// Create a container and set it to the material we just defined
-	Ogre::OverlayContainer* imageContainer = static_cast<Ogre::OverlayContainer*>(overlayManager->createOverlayElement("Panel", "DisplayImage1"));
-	imageContainer->setMaterialName("ImageMaterial");
-	imageContainer->setMetricsMode(Ogre::GMM_RELATIVE);
-	imageContainer->setDimensions(1, 1);
-	imageContainer->setPosition(0, 0);
+		// Ceiling rigidbody
+		btBoxShape* ceilingShape = new btBoxShape(btVector3(50.0f, 5.0f, 50.0f));
+		origin = convert(ceilingNode->getPosition());
+		rotation = convert(ceilingNode->getOrientation());
+		transform.setIdentity();
+		transform.setOrigin(btVector3(origin.x(), origin.y() + 5, origin.z()));
+		transform.setRotation(rotation);
+		mBulletContext.CreateRigidBody(0.0f, transform, ceilingShape, node);
 
-	// Add the container to the overlay
-	mSniperScopeOverlay->add2D(imageContainer);
-	// Display the overlay
-	//overlay->show();
+		// Wall rigidbody
+		btBoxShape* wallShape[4];
+		// Front wall
+		wallShape[0] = new btBoxShape(btVector3(50.0f, 20.0f, 5.0f));
+		origin = convert(wallNodes[0]->getPosition());
+		rotation = convert(wallNodes[0]->getOrientation());
+		transform.setIdentity();
+		transform.setOrigin(btVector3(origin.x(), origin.y(), origin.z() - 5));
+		transform.setRotation(rotation);
+		mBulletContext.CreateRigidBody(0.0f, transform, wallShape[0], node);
+
+		// Right wall
+		wallShape[1] = new btBoxShape(btVector3(50.0f, 20.0f, 5.0f));
+		origin = convert(wallNodes[1]->getPosition());
+		rotation = convert(wallNodes[1]->getOrientation());
+		transform.setIdentity();
+		transform.setOrigin(btVector3(origin.x() + 5, origin.y(), origin.z()));
+		transform.setRotation(rotation);
+		mBulletContext.CreateRigidBody(0.0f, transform, wallShape[1], node);
+
+		// Left wall
+		wallShape[2] = new btBoxShape(btVector3(50.0f, 20.0f, 5.0f));
+		origin = convert(wallNodes[2]->getPosition());
+		rotation = convert(wallNodes[2]->getOrientation());
+		transform.setIdentity();
+		transform.setOrigin(btVector3(origin.x() - 5, origin.y(), origin.z()));
+		transform.setRotation(rotation);
+		mBulletContext.CreateRigidBody(0.0f, transform, wallShape[2], node);
+
+		// Back wall
+		wallShape[3] = new btBoxShape(btVector3(50.0f, 20.0f, 5.0f));
+		origin = convert(wallNodes[3]->getPosition());
+		rotation = convert(wallNodes[3]->getOrientation());
+		transform.setIdentity();
+		transform.setOrigin(btVector3(origin.x(), origin.y(), origin.z() + 5));
+		transform.setRotation(rotation);
+		mBulletContext.CreateRigidBody(0.0f, transform, wallShape[3], node);
+	}
+
+	// Place objects in scene
+	{
+		// Create a row of boxes
+		int boxCount = 0;
+
+		for (int i = -20; i <= 20; i += 5)
+		{
+			char entityName[8];
+			sprintf(entityName, "cube%d", boxCount++);
+
+			// Create cube mesh with unique name
+			Ogre::Entity* cube = mSceneMgr->createEntity(entityName, "cube.mesh");
+			cube->setMaterialName("Examples/BumpyMetal");
+			cube->setCastShadows(true);
+			auto node = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+			node->attachObject(cube);
+			// Scale it to appropriate size
+			node->scale(0.04, 0.04, 0.04);
+
+			// Create a collision shape
+			// Note that the size should match the size of the object that will be displayed
+			btCollisionShape* collisionShape = new btBoxShape(btVector3(2, 2, 2));
+
+			// The object's starting transformation
+			btTransform startingTrans;
+			startingTrans.setIdentity();
+			startingTrans.setOrigin(btVector3(i, 10, 10));
+			startingTrans.setRotation(btQuaternion(0, 0, 0, 1));
+
+			// Create the rigid body
+			btRigidBody* rigidBody = mBulletContext.CreateRigidBody(1.0f, startingTrans, collisionShape, node);
+
+			// Set the rigid body's friction
+			rigidBody->setFriction(0.9);
+		}
+	}
+
+	// Setup FPS player
+	{
+		auto fpsPlayer = sceneMgr->getRootSceneNode()->createChildSceneNode("FPSPlayer");
+		auto cameraNode = fpsPlayer->createChildSceneNode("CameraNode");
+		cameraNode->attachObject(mCamera);
+		mCamera->setPosition(0.0f, 0.0f, 0.0f);
+		cameraNode->setPosition(0.0f, 10.0f, 0.0f);
+		mFPSController.Attach(cameraNode, fpsPlayer);
+		mWeapon.SetState(new BallShooter(mSceneMgr, mCamera, &mBulletContext));
+	}
+
+	// Draw crosshair
+	{
+		// Create a manual object for 2D
+		Ogre::ManualObject* manual = mSceneMgr->createManualObject("manual");
+
+		// Use identity view/projection matrices
+		manual->setUseIdentityProjection(true);
+		manual->setUseIdentityView(true);
+
+		manual->begin("BaseWhiteNoLighting", Ogre::RenderOperation::OT_LINE_LIST);
+
+		manual->position(-0.05, 0.0, 0.0);
+		manual->position(0.05, 0.0, 0.0);
+		manual->position(0.0, -0.05, 0.0);
+		manual->position(0.0, 0.05, 0.0);
+
+		manual->end();
+
+		// Use infinite AAB to always stay visible
+		Ogre::AxisAlignedBox aabInf;
+		aabInf.setInfinite();
+		manual->setBoundingBox(aabInf);
+
+		// Render just before overlays
+		manual->setRenderQueueGroup(Ogre::RENDER_QUEUE_OVERLAY - 1);
+
+		// Attach to scene
+		mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(manual);
+	}
+
+	// Setup overlay images
+	{
+		// Get the pointer to the overlay manager
+		Ogre::OverlayManager* overlayManager = Ogre::OverlayManager::getSingletonPtr();
+
+		// Create an overlay
+		mSniperScopeOverlay = overlayManager->create("Overlay");
+
+		// Load an image into Ogre's texture manager
+		Ogre::Image image;
+		image.load("sniper-scope-cropped.png", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+		Ogre::TextureManager::getSingleton().loadImage("LogoTexture", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, image, Ogre::TEX_TYPE_2D, 0, 1.0f);
+
+		// Already loaded to texture, no longer need image
+		image.freeMemory();
+
+		// Create a material and assign the texture to the material
+		Ogre::MaterialPtr materialPtr = Ogre::MaterialManager::getSingleton().create("ImageMaterial", "General");
+		materialPtr->getTechnique(0)->getPass(0)->createTextureUnitState("LogoTexture");
+		materialPtr->getTechnique(0)->getPass(0)->setSceneBlending(Ogre::SceneBlendType::SBT_TRANSPARENT_ALPHA);
+
+		// Create a container and set it to the material we just defined
+		Ogre::OverlayContainer* imageContainer = static_cast<Ogre::OverlayContainer*>(overlayManager->createOverlayElement("Panel", "DisplayImage1"));
+		imageContainer->setMaterialName("ImageMaterial");
+		imageContainer->setMetricsMode(Ogre::GMM_RELATIVE);
+		imageContainer->setDimensions(1, 1);
+		imageContainer->setPosition(0, 0);
+
+		// Add the container to the overlay
+		mSniperScopeOverlay->add2D(imageContainer);
+	}
 }
